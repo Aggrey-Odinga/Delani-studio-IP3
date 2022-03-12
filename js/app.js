517140840af7a057e8c7b2e17f7c967a8f2cdf92
Aggrey Odinga <aggrey.odinga@student.moringaschool.com>
2022-03-12 22:11:11 +0300

    Add toggle effect to what we do section

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(".design").on('click', function(event){
+console.log("designclick")
+$("#design-text").show();
+});
+// $(document).on("click",".design",function(event) {
+//     console.log("designclick")
+// $("div" ).show();
+// });
+
+$(document).ready(function(){
+ $("#design-text").hide();
+});
